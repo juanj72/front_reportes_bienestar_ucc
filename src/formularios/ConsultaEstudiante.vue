11dac8a17d33535f50d4fbe4284f15d2558caa8f
juan jose <juan.jara@campusucc.edu.co>
2023-04-20 12:14:04 -0500

    agregacion ventana de consulta estudiante

--- src/formularios/ConsultaEstudiante.vue
+++ src/formularios/ConsultaEstudiante.vue
@@ -9,18 +9,30 @@
   <div v-if="datos">
     
     <div style="display:flex; justify-content:center;">
-      <div style="width:100%; margin: 2%; background-color:#fff; box-shadow:0px 2px 6px rgba(0,0,0,0.3); padding:20px;">
+      <div style="width:100%; margin: 2%; background-color:#fff; box-shadow:0px 2px 6px rgba(0,0,0,0.3); padding:20px;" >
         <button class="douwnload-btn">descargar informacion</button>
-        <h2 style="text-align:center;">Estudiante</h2>
        
-        <p style="text-align:center;">informacion del estudiante</p>
-        <h3>ID: {{ datos.idEstudiante }}</h3>
-        <h3>Perfil: {{ datos.Perfil_idPerfil }}</h3>
-        <h3>Programa: {{ datos.Programa_idPrograma }}</h3>
-        <h3>Nombre: {{ datos.nombre }}</h3>
-        <h3>Apellido: {{ datos.apellido }}</h3>
-        <h3>Telefono: {{ datos.telefono }}</h3>
-        <h3>Codigo: {{ datos.codigo }}</h3>
+       <div v-for="dato in datos" :key="dato.idEstudiante">
+        <h1 style="text-align:center;">Detalle estudiante</h1>
+        <hr>
+        <img src="../assets/logotipo.svg" alt="">
+        <hr>
+        <hr>
+        <h2 style="text-align:center;">informacion del estudiante</h2>
+        <h3>ID: {{ dato.idEstudiante }}</h3>
+        <h3>Perfil: {{ dato.nombre }}</h3>
+        <h3>Programa: {{ dato.apellido }}</h3>
+        <h3>Nombre: {{ dato.usuario }}</h3>
+        <h3>Apellido: {{ dato.correo }}</h3>
+        <h3>Programa: {{ dato.nombre_programa }}</h3>
+        <h3>Total de horas eventos: {{ dato.horas_evento }}</h3>
+        <h3>Total horas por actividades: {{ dato.horas_actividad }}</h3>
+        <h3>Total de horas bienestar: {{ dato.total_horas }}</h3>
+        <hr>
+       </div>
+        
+
+       
 
       </div>
     </div>
@@ -50,14 +62,14 @@ export default {
 
   methods: {
     submitForm() {
-      axios.get('http://127.0.0.1:8000/api/estudiantes/' + this.idEstudiante.id, {})
+     console.log( axios.get('http://127.0.0.1:8000/api/informacion_estudiante/' + this.idEstudiante.id, {})
         .then(response => {
           this.datos = response.data;
           console.log(this.idEvento)
         })
         .catch(error => {
           console.log(error);
-        });
+        }))
     },
   },
 
