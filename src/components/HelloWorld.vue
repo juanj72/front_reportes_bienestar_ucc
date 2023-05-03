b43aee47914d08e2173848f88e3922742dea6bb2
juan jose <danielasilvatejedor@gmail.com>
2023-05-03 10:27:49 -0500

    graficas de panel inicial

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,37 @@
 <template >
+<h3>Bienvenido al modulo de reportes</h3>
 
-  <h1>Modulo de reportes</h1>
-  <p>Aquí encontraras todo lo relacionado a los reportes de horas por Estudiante.</p>
-  <CardPrueba dato_prueba="hola undo desde el componente pero card"></CardPrueba>
-  <CardPrueba dato_prueba="hola undo desde el componente pero 2"></CardPrueba>
-  <div>
-    <i class="fas fa-user"></i>
-    <i class="far fa-envelope"></i>
+
+  <div class="card-container">
+    <div class="card">
+      <div class="card-header">Asistencia por horas en actividades</div>
+      <div class="card-body" v-if="actividades.length > 0">
+        <GraficasPanel :datos_h="actividades" ></GraficasPanel>
+
+      </div> 
+    </div>
+
+    <div class="card">
+      <div class="card-header">Cantidad de estudiantes por evento</div>
+      <div class="card-body" v-if="eventos.length > 0">
+        <GraficasPanel :datos_h="eventos" ></GraficasPanel>
+
+      </div> 
+    </div>
+
+    <div class="card">
+      <div class="card-header">Card 3</div>
+      <div class="card-body">This is the content of card 3.</div> 
+    </div>
   </div>
+
+
 
 </template>
 
 <script>
-
-import CardPrueba from '../cards/CardPrueba'
-
-
-
-
+import axios from 'axios';
+import GraficasPanel from '@/graficas/GraficasPanel.vue';
 
 export default {
   name: 'HelloWorld',
@@ -25,27 +39,53 @@ export default {
     msg: String
   },
   components:{
-    CardPrueba,
+    GraficasPanel
    
    
+},
+data:function(){
+ return {
+  actividades:[],
+  eventos:[]
+  
+ }
+},
+mounted:function(){
+
+  console.log(axios.get('http://127.0.0.1:8000/api/asistenciaActividades').then(response => this.actividades = response.data).catch(error => console.log(error)));
+  console.log(axios.get('http://127.0.0.1:8000/api/asistenciaEventos').then(response => this.eventos = response.data).catch(error => console.log(error)));
+
 }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+.card-container {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.card {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 2px 2px rgba(0, 0, 0, 0.3);
+  margin-right: 10px;
+  width: 80%;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.card-header {
+  background-color: #f2f2f2;
+  font-weight: bold;
+  padding: 10px;
 }
-a {
-  color: #42b983;
+
+.card-body {
+  padding: 10px;
 }
+
 </style>
