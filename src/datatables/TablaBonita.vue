4221ea13d22e73e953c3bcbf031b033705ee5e9a
juan jose <danielasilvatejedor@gmail.com>
2023-05-03 22:53:59 -0500

    datatable instalado por fin

--- src/datatables/TablaBonita.vue
+++ src/datatables/TablaBonita.vue
@@ -1,33 +1,37 @@
 <template>
-<div>hola mundo</div>
-</template>
+    <div>
+      <DataTable :data="myData" class="display">
+        <thead>
+          <tr>
+            <th>Columna 1</th>
+            <th>Columna 2</th>
+          </tr>
+        </thead>
+      </DataTable>
+    </div>
+  </template>
+  
+  <script>
+  import DataTable from 'datatables.net-vue3';
 
-<script>
-// import DataTable from 'datatables.net-vue3';
-// import DataTablesCore from 'datatables.net';
+import DataTablesLib from 'datatables.net';
 
-
-// DataTable.use(DataTablesCore);
-
-
-
-export default {
-    name: 'TablaBonita',
+DataTable.use(DataTablesLib);
+  export default {
+    name:'TablaBonita',
     components: {
-        // DataTable
+      DataTable,
     },
-    data: () => {
-        return {
-            datos: null,
-            base: []
-        }
-
+    data() {
+      return {
+        myData: [
+          ['Fila 1, Columna 1', 'Fila 1, Columna 2'],
+          ['Fila 2, Columna 1', 'Fila 2, Columna 2'],
+        ],
+      };
     },
-
-
-
-
-
-}
-
-</script>
+    mounted:function(){
+        
+    }
+  };
+  </script>
